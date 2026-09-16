f7a83bde713dccffcc247255ca79025385c04959
JJAM1025 <jamancao2030@clc.pshs.edu.ph>
2026-09-16 21:50:19 +0800

    Create q1_sg5_a1_Pinatubo_Mancao.py
    sg5 activity

---
+++ q1/q1_sg5_a1_Pinatubo_Mancao.py
@@ -0,0 +1,13 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+    def take_damage(self, amount):
+        self.hp = self.hp - amount
+
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+arthur.take_damage(10)
+print("Arthur HP:", arthur.hp)
+print("Morgana HP:", morgana.hp)
