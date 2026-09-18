f182a9028fbd306ba80a534fabb247756df67b4b
JJAM1025 <jamancao2030@clc.pshs.edu.ph>
2026-09-18 08:08:38 +0800

    Update q1_sg6_Pinatubo_Mancao.py
    made the spacing more uniform to the other codes

--- q1/q1_sg6_Pinatubo_Mancao.py
+++ q1/q1_sg6_Pinatubo_Mancao.py
@@ -2,15 +2,12 @@ class Lab:
     def __init__(self, room_number):
         self.room_number = room_number
 
-
 class Technician:
     def __init__(self, name):
         self.name = name
-        self.assigned_lab = None
-    
+        self.assigned_lab = None  
     def assign_lab(self, lab_obj):
         self.assigned_lab = lab_obj
-
 
 chem_lab = Lab("302")
 mr_cruz = Technician("Mr. Cruz")
